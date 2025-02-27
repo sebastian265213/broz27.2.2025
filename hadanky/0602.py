f8475b588dbdc9908e2a2d6c8cee650a1cf8a1d4
broz265213 <sebastian.broz@seznam.cz>
2025-02-27 16:33:00 +0100

    hadanky

---
+++ hadanky/0602.py
@@ -0,0 +1,44 @@
+body = 0
+print ("jaky telefon je a byl legendární")
+print ("a) tvoje mama (tondo)")
+print ("b) nokia 3310")
+print ("c) nokia 3210")
+odpoved  = input()
+if odpoved == "b":
+    body += 1
+    print ("spravne")
+else :
+    print ("spatne")
+print ("46458")
+print ("a) 646445")
+print  ("b) 46458")
+print ("c) 54452")
+odpoved  = input()
+if odpoved == "b":
+    body += 1
+    print ("spravne")
+else :
+    print ("spatne")
+print ("co je nejnovejsi iphone na trhu") 
+print ("a) 17")
+print ("b) 14")
+print ("c) 16")
+odpoved  = input()
+if odpoved == "c":
+    body += 1
+    print ("spravne")
+else :
+    print ("spatne")
+print ("co mam ted v kapse") 
+print ("a) mobil")
+print ("b) penezenku")
+print ("c) odpatky")
+
+odpoved  = input()
+if odpoved == "c":
+    body += 1
+    print ("spravne")
+else :
+    print ("spatne")
+print ("ziskal jsi bodu:", body)
+print ("max body 4")
